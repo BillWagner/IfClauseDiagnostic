84f1cbfa529ad94189c25ee017c3b6a4e6141ea7
Bill Wagner <bill.w.wagner@outlook.com>
2014-12-11 13:25:07 -0500

    update the first test to find and fix b1
    This is a simple if statement without braces.

--- IfClauseDiagnostic/IfClauseDiagnostic.Test/UnitTests.cs
+++ IfClauseDiagnostic/IfClauseDiagnostic.Test/UnitTests.cs
@@ -23,27 +23,34 @@ namespace IfClauseDiagnostic.Test
 
         //Diagnostic and CodeFix both triggered and checked for
         [TestMethod]
-        public void TestMethod2()
+        public void IfStatementWithoutBraces()
         {
             var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    namespace ConsoleApplication1
+namespace SampleCode
+{
+    class Program
     {
-        class TypeName
-        {   
+        static void Main(string[] args)
+        {
+            bool b1 = true;
+
+            if (b1)
+                Console.WriteLine(""b1"");
+
         }
-    }";
+    }
+}";
             var expected = new DiagnosticResult
             {
                 Id = IfClauseDiagnosticAnalyzer.DiagnosticId,
-                Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
-                Severity = DiagnosticSeverity.Warning,
+                Message = "if clauses should be surrounded by braces.",
+                Severity = DiagnosticSeverity.Info,
                 Locations =
                     new[] {
                             new DiagnosticResultLocation("Test0.cs", 11, 15)
@@ -53,19 +60,27 @@ namespace IfClauseDiagnostic.Test
             VerifyCSharpDiagnostic(test, expected);
 
             var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    namespace ConsoleApplication1
+namespace SampleCode
+{
+    class Program
     {
-        class TYPENAME
-        {   
+        static void Main(string[] args)
+        {
+            bool b1 = true;
+
+            if (b1)
+            {
+                Console.WriteLine(""b1"");
+            }
         }
-    }";
+    }
+}";
             VerifyCSharpFix(test, fixtest);
         }
 
