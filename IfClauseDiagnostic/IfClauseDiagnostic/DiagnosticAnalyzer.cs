c0a16fa66c666930538999fc7c25687881f4083e
Bill Wagner <bill.w.wagner@outlook.com>
2014-12-15 11:14:42 -0500

    Update Initialize to look for if blocks
    This will trigger our diagnostic analysis on the correct node.

--- IfClauseDiagnostic/IfClauseDiagnostic/DiagnosticAnalyzer.cs
+++ IfClauseDiagnostic/IfClauseDiagnostic/DiagnosticAnalyzer.cs
@@ -24,23 +24,13 @@ namespace IfClauseDiagnostic
 
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction(AnalyzeIfBlock, SyntaxKind.IfStatement);
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        private static void AnalyzeIfBlock(SyntaxNodeAnalysisContext context)
         {
             // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-            {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-                context.ReportDiagnostic(diagnostic);
-            }
+            var statement = context.Node;
         }
     }
 }
